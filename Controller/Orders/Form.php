990ecec5cfb8bc660af4fcf796b2f192ca8a313f
Alex <alex@magestore.com>
2017-06-16 16:47:44 +0700

    fix di compile issue

--- Controller/Orders/Form.php
+++ Controller/Orders/Form.php
@@ -9,35 +9,10 @@ namespace Magestore\OrderCheckingSuccess\Controller\Orders;
 class Form extends \Magento\Framework\App\Action\Action
 {
     /**
-     * @var \Magento\Framework\View\Result\RedirectFactory
-     */
-    protected $redirectFactory;
-
-    /**
-     * Form constructor.
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
-     * @param \Magento\Framework\App\Cache\StateInterface $cacheState
-     * @param \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
-     * @param \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
-     */
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
-        \Magento\Framework\App\Cache\StateInterface $cacheState,
-        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool,
-        \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
-    )
-    {
-        parent::__construct($context);
-        $this->redirectFactory = $redirectFactory;
-    }
-
-    /**
      * @return $this
      */
     public function execute()
     {
-        return $this->redirectFactory->create()->setPath('ordercheckingsuccess/orders');
+        return $this->resultRedirectFactory->create()->setPath('ordercheckingsuccess/orders');
     }
 }
--- Controller/Orders/History.php
+++ Controller/Orders/History.php
@@ -9,35 +9,10 @@ namespace Magestore\OrderCheckingSuccess\Controller\Orders;
 class History extends \Magento\Framework\App\Action\Action
 {
     /**
-     * @var \Magento\Framework\View\Result\RedirectFactory
-     */
-    protected $redirectFactory;
-
-    /**
-     * History constructor.
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
-     * @param \Magento\Framework\App\Cache\StateInterface $cacheState
-     * @param \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
-     * @param \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
-     */
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
-        \Magento\Framework\App\Cache\StateInterface $cacheState,
-        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool,
-        \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
-    )
-    {
-        parent::__construct($context);
-        $this->redirectFactory = $redirectFactory;
-    }
-
-    /**
      * @return \Magento\Framework\View\Result\Page
      */
     public function execute()
     {
-        return $this->redirectFactory->create()->setPath('sales/order/history');
+        return $this->resultRedirectFactory->create()->setPath('sales/order/history');
     }
 }
